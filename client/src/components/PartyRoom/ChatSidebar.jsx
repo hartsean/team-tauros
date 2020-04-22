16e7bfb9c80e61461d2e1897e9adea92d00456aa
hartsean <hartsean@gmail.com>
2020-04-22 12:59:00 -0500

    (feat) render game rules and assign character buttons to sidebar"

--- client/src/components/PartyRoom/ChatSidebar.jsx
+++ client/src/components/PartyRoom/ChatSidebar.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ChatPeopleList from './ChatPeopleList.jsx';
 import ChatDetails from './ChatDetails.jsx';
+import GameRules from './GameRules.jsx';
+import AssignCharacter from './AssignCharacter.jsx';
 
 const ChatSidebar = ({ users, partyInfo }) => {
   return (
     <div className="container">
       <ChatPeopleList users={users} />
       <ChatDetails partyInfo={partyInfo} />
+      <GameRules />
+      <AssignCharacter users={users} />
     </div>
   );
 }
