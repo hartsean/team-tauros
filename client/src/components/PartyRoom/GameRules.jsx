5696cf2258fcfdc41756aecfa2948fdd38c2aa5a
hartsean <hartsean@gmail.com>
2020-04-22 13:30:37 -0500

    (refactor)removed excess text

--- client/src/components/PartyRoom/GameRules.jsx
+++ client/src/components/PartyRoom/GameRules.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 
-const GameRules = ({}) => {
+const GameRules = ({ }) => {
   return (
     <div>
       <h3>Game Rules</h3>
       <a href="https://icebreakerideas.com/who-am-i-game/" target="_blank"><h5>Learn More</h5></a>
-      <ul>
-        <li>1. Learn how to play (see above)</li>
-        <li>2. Click Assign Character </li>
-        <li>3. Guess who you are by asking questions!</li>
 
-      </ul> 
+        1. Guess who you are by asking questions!
+
     </div>
   );
 }
