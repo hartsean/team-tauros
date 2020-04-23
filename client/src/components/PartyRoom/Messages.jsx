5c9751311e85a583218dbdff67bbfd49603b5865
hartsean <hartsean@gmail.com>
2020-04-23 10:57:44 -0500

    (feat) import ImageUpload Component

--- client/src/components/PartyRoom/Messages.jsx
+++ client/src/components/PartyRoom/Messages.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ChatMessage from './ChatMessage.jsx';
 import ChatInput from './ChatInput.jsx';
+import ImageUpload from './ImageUpload.jsx';
 
 import { Link } from 'react-router-dom';
 
@@ -11,6 +12,7 @@ const Messages = ({ messages, message, sendMessage, setMessage, leftParty }) => 
       <ChatMessage messages={messages} />
       </div>
       <ChatInput message={message} sendMessage={sendMessage} setMessage={setMessage} />
+      <ImageUpload />
       <Link to={{ pathname: '/parties' }}>
         <button type="button" className="btn btn-primary" onClick={leftParty}>Leave Party</button>
       </Link>
